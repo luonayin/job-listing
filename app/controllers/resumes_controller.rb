8a8d4da0f7702b7ca18615dc57ef3fa8015ca5f7
你的名字 <email@xxx.xxx>
2017-02-23 14:11:03 +0800

    step 7 upload resume

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -8,7 +8,7 @@ class ResumesController < ApplicationController
 
   def create
     @job = Job.find(params[:job_id])
-    @resume = Rresume.new(resume_params)
+    @resume = Resume.new(resume_params)
     @resume.job = @job
     @resume.user = current_user
 
@@ -23,6 +23,6 @@ class ResumesController < ApplicationController
   private
 
   def resume_params
-    params.require(:resume).permit(:content)
+    params.require(:resume).permit(:content, :attachment)
   end
 end
