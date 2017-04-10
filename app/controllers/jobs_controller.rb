d25accdfed1aeac255cdc3effc1b30606c138e61
你的名字 <email@xxx.xxx>
2017-04-10 08:18:57 +0800

    implement step 5

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,6 +1,6 @@
 class JobsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
-  
+
 
   def index
     @jobs = Job.where(:is_hidden => false).order("created_at DESC")
@@ -12,6 +12,11 @@ class JobsController < ApplicationController
 
   def show
     @job = Job.find(params[:id])
+
+    if @job.is_hidden
+      flash[:warning] = "This Job already archieved"
+      redirect_to root_path
+    end
   end
 
   def create
